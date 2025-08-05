132b8d0166745648ccca620aa37546886a6c2e35
tushar-41 <tusharkmr4126@gmail.com>
2025-08-05 22:26:28 +0530

    updated login api

--- backend/controllers/userController.cjs
+++ backend/controllers/userController.cjs
@@ -1,11 +1,13 @@
-// import {user} from '../models/user';
+import {user} from '../models/user';
 import bcrypt from "bcryptjs";
 
 const login = (async(req,res) => {
     const {name,email,password = 'secret'} = req.body;
     const salt = bcrypt.genSyncSalt(10);
     const hashedPassword = bcrypt.hashSync(password,salt);
-    console.log(hashedPassword);
+    const newUser = await user({name,email,password});
+    await newUser.save();
+    res.send({name,email,password});
 });
 
-login();
+export default login;
