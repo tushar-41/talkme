9aacd2940670e735081caf654ffb621017ce878a
tushar-41 <tusharkmr4126@gmail.com>
2025-08-12 23:15:32 +0530

    UserController updated

--- backend/controllers/userController.cjs
+++ backend/controllers/userController.cjs
@@ -1,13 +1,25 @@
-import {user} from '../models/user';
+import {User} from '../models/user';
 import bcrypt from "bcryptjs";
 
-const login = (async(req,res) => {
+export const login = async(req,res) => {
     const {name,email,password = 'secret'} = req.body;
+    const user = await User.findOne({email});
+    const isMatched = bcrypt.compare(password,user.password);
+    if(isMatched){
+        console.log('User detected successfully');
+    }else{
+        console.log('User not registered yet');
+    }
+    
+};
+
+export const register = async(req,res) => {
+    const {name,password,email} = req.body;
     const salt = bcrypt.genSyncSalt(10);
     const hashedPassword = bcrypt.hashSync(password,salt);
-    const newUser = await user({name,email,password});
+    const newUser = await User({name,email,password});
+    //Or newUser = new User({name:name,email:email,password:password});
+    //newUser.save();
     await newUser.save();
     res.send({name,email,password});
-});
-
-export default login;
+}
